0f5cf050f4c44b67d250cc960d59c6414ba1466d
badlogic <badlogicgames@gmail.com>
2020-07-13 02:17:06 +0200

    More WASM fun.

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 SOURCES=`find src -name *.cpp -maxdepth 1`
-emcc -s WASM=1 -DWASM -s LLD_REPORT_UNDEFINED -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' -Isrc $SOURCES -o qak.js
+emcc -O3 -s WASM=1 -DWASM \
+	 -s LLD_REPORT_UNDEFINED \
+	 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' \
+	 -Isrc $SOURCES \
+	 -o qak.js
